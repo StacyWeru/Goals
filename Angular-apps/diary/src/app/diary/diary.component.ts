59f5b592500bec3226bd236ae30ab2d1297f79af
StacyWeru <stacyweru@gmail.com>
2020-09-27 12:10:34 +0300

    Finally a commit without errors

--- Angular-apps/diary/src/app/diary/diary.component.ts
+++ Angular-apps/diary/src/app/diary/diary.component.ts
@@ -9,7 +9,7 @@ import { Diary } from "../diary";
 export class DiaryComponent implements OnInit {
   name ="Stacy";
 
-  notes:Diary[] = [
+  diarys:Diary[] = [
     { title:'Got hit by a car',date:(20121023),entry:'Didnt end well,now I am dead'},
     { title:'Got rushed to the hospital',date:(20121023),entry:' Again Didnt end well,cause now I am dead...Feel bad for the doctors though'},
     { title:'Realized that I am a ghost',date:(20121023),entry:' I have to say that I am embarassed that I had a couple temper tantrums,the hospital had to replace a couple of windows'},
