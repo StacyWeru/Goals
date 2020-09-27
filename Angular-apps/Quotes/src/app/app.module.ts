d470f724ba6c715473b37f889a5848425312be2a
StacyWeru <stacyweru@gmail.com>
2020-09-28 02:27:26 +0300

    Added the necessary cmponents so far

--- Angular-apps/Quotes/src/app/app.module.ts
+++ Angular-apps/Quotes/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotePageComponent } from './quote-page/quote-page.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotePageComponent,
+    QuoteFormComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
