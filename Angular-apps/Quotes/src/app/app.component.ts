f0685f87a91b09239525f731d4263a4e83b22125
StacyWeru <stacyweru@gmail.com>
2020-09-28 15:51:28 +0300

    Fixed all the errors

--- Angular-apps/Quotes/src/app/app.component.ts
+++ Angular-apps/Quotes/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {} from './quote'
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
--- Angular-apps/Quotes/src/app/quote-page/quote-page.component.ts
+++ Angular-apps/Quotes/src/app/quote-page/quote-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote} from '../quote'
+import { Quote} from '../quote';
 
 @Component({
   selector: 'app-quote-page',
@@ -8,12 +8,12 @@ import { Quote} from '../quote'
 })
 export class QuotePageComponent implements OnInit {
 
-  quotes=[
+  quotes : Quote[]=[
      new Quote(1,'Kim Namjoon','Maybe I made a mistake yesterday,but yesterdays me is still me,I am who I am today with all my faults.Tomorrow I might be a tiny it wiser but that is me too','Ace',new Date(2018,10,12)),
-     new Quote(2,'Min Yoongi','In the end,some of you greatest pains becomes your greatest strengths','Ace',new Date(2018,4,09)),
+     new Quote(2,'Min Yoongi','In the end,some of you greatest pains becomes your greatest strengths','Ace',new Date(2018,4,9)),
      new Quote(3,'Kim Seokjin','Youre really the only one who needs to acknowledge your effort','Ace', new Date(2015,11,4)),
      new Quote(1,'Jung Hoseok','Lets not push off the things we need to do today for tomorrow','Ace', new Date(2018,3,18))
-  ]
+  ];
 
 
   addNewQuote(quote) {
