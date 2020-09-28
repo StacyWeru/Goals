1567936f4da2db43d6a22828ec21fe0275573409
StacyWeru <stacyweru@gmail.com>
2020-09-28 18:29:19 +0300

    Final Commit

--- Angular-apps/Quotes/src/app/quote-page/quote-page.component.ts
+++ Angular-apps/Quotes/src/app/quote-page/quote-page.component.ts
@@ -9,7 +9,7 @@ import { Quote} from '../quote';
 export class QuotePageComponent implements OnInit {
 
   quotes : Quote[]=[
-     new Quote(1,'Kim Namjoon','Maybe I made a mistake yesterday,but yesterdays me is still me,I am who I am today with all my faults.Tomorrow I might be a tiny it wiser but that is me too','Ace',new Date(2018,10,12)),
+     new Quote(1,'Kim Namjoon','Maybe I made a mistake yesterday,but yesterdays me is still me,I am who I am today with all my faults.Tomorrow I might be a tiny bit wiser but that is me too','Ace',new Date(2018,10,12)),
      new Quote(2,'Min Yoongi','In the end,some of you greatest pains becomes your greatest strengths','Ace',new Date(2018,4,9)),
      new Quote(3,'Kim Seokjin','Youre really the only one who needs to acknowledge your effort','Ace', new Date(2015,11,4)),
      new Quote(1,'Jung Hoseok','Lets not push off the things we need to do today for tomorrow','Ace', new Date(2018,3,18))
